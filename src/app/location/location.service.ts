fff6b7060e94e7920fbb101fa7b54f536597b5d8
Cristian Greco <cristian.greco@itv.com>
2016-07-31 20:04:05 +0100

    Add Location service for fetching current location

---
+++ src/app/location/location.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Coordinate } from '../shared/coordinate';
+import { Observable, Observer } from 'rxjs/Rx';
+
+@Injectable()
+export class LocationService {
+
+  getCurrentPosition(): Observable<Coordinate> {
+    return Observable.create((observer: Observer<Coordinate>) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            const coordinate: Coordinate = {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude
+            };
+            observer.next(coordinate);
+            observer.complete();
+          },
+          error => observer.error(error.message));
+      } else {
+        observer.error('Geolocation is not supported or is disabled on this device');
+      }
+    });
+  }
+}
--- src/app/nearby-pokemon.component.ts
+++ src/app/nearby-pokemon.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from './pokemon/pokemon.service';
 import { Pokemon } from './shared';
+import {LocationService} from './location/location.service';
+import {Coordinate} from './shared/coordinate';
 
 @Component({
   moduleId: module.id,
   selector: 'nearby-pokemon-app',
   templateUrl: 'nearby-pokemon.component.html',
-  providers: [PokemonService]
+  providers: [PokemonService, LocationService]
 })
 export class NearbyPokemonAppComponent implements OnInit {
 
-  constructor(private pokemonService: PokemonService) { }
+  constructor(private pokemonService: PokemonService, private locationService: LocationService) { }
 
   ngOnInit() {
     const latitude: number = 51.497121994573;
     const longitude: number = -0.12494802474976;
 
     this.pokemonService.fetchPokemon(latitude, longitude).subscribe((pokemon: Pokemon[]) => console.log(pokemon));
+    this.locationService.getCurrentPosition().subscribe((coords: Coordinate) => console.log(coords));
   }
 }
